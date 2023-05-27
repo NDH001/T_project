2b06fd2a3ac872f46adb11aa2cce305018f28f0f
Zhi Jun <zhi_jun95@hotmail.com>
2023-05-27 13:12:40 +0800

    update all

--- src/main.py
+++ src/main.py
@@ -112,8 +112,8 @@ def main(path, epochs=30, l_r=0.001, splits=5, report=False):
 if __name__ == "__main__":
     main(
         r"WA_Fn-UseC_-Telco-Customer-Churn.csv",
-        1,
+        30,
         0.001,
-        2,
+        5,
         True,
     )
